3145a730a806a0af5839c7882c8532947f95eaad
Kawakaze-Kurumi <hung0986314127@example.com>
2025-06-01 07:52:31 +0700

    ok

--- PND_WEB/Models/Quotation.cs
+++ PND_WEB/Models/Quotation.cs
@@ -7,35 +7,48 @@ namespace PND_WEB.Models;
 public partial class Quotation
 {
     [Key]
+    [Display(Name = "Mã báo giá")]
     public string QuotationId { get; set; } = null!;
 
+    [Display(Name = "Trạng thái")]
     public string? Qsatus { get; set; }
 
+    [Display(Name = "Nhân viên")]
     public string? StaffName { get; set; }
 
+    [Display(Name = "Liên lạc nhân viên")]
     public string? Contact { get; set; }
 
-
+    [Display(Name = "Ngày tạo")]
     [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
     public DateTime? Qdate { get; set; }
 
+    [Display(Name = "Khách hàng")]
     public string? CusTo { get; set; }
 
+    [Display(Name = "Liên hệ khách hàng")]
     public string? CusContact { get; set; }
 
+    [Display(Name = "Hiệu lực")]
     [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
     public DateTime? Valid { get; set; }
 
+    [Display(Name = "Điều khoản")]
     public string? Term { get; set; }
 
+    [Display(Name = "Khối lượng")]
     public string? Vol { get; set; }
 
+    [Display(Name = "Hàng hóa")]
     public string? Commodity { get; set; }
 
+    [Display(Name = "Điểm đi (POL)")]
     public string? Pol { get; set; }
 
+    [Display(Name = "Địa chỉ")]
     public string? Adds { get; set; }
 
+    [Display(Name = "Điểm đến (POD)")]
     public string? Pod { get; set; }
 
     public virtual ICollection<QuotationsCharge> QuotationsCharges { get; set; } = new List<QuotationsCharge>();
--- PND_WEB/Models/QuotationsCharge.cs
+++ PND_WEB/Models/QuotationsCharge.cs
@@ -11,16 +11,22 @@ public partial class QuotationsCharge
 
     public string? QuotationId { get; set; }
 
+    [Display(Name = "Tên phí")]
     public string? ChargeName { get; set; }
 
+    [Display(Name = "Số lượng")]
     public double? Quantity { get; set; }
 
+    [Display(Name = "Đơn vị")]
     public string? Unit { get; set; }
 
+    [Display(Name = "Giá")]
     public double? Rate { get; set; }
 
+    [Display(Name = "Tiền tệ")]
     public string? Currency { get; set; }
 
+    [Display(Name = "Ghi chú")]
     public string? Notes { get; set; }
 
     public virtual Quotation? Quotation { get; set; }
